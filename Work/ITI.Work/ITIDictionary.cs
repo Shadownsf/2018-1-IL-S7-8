842cf6bd856a1c697f09cbf98b06e0a45dd905f2
Pierre KORE <pierre.kore@infeeny.com>
2018-04-13 16:36:47 +0200

    first try disctionnary

--- Work/ITI.Work/ITIDictionary.cs
+++ Work/ITI.Work/ITIDictionary.cs
@@ -10,29 +10,72 @@ namespace ITI.Work
         int _count;
         Node[] _buckets;
 
+        public ITIDictionary()
+        {
+            _count = 0;
+            _buckets = new Node[7];
+        }
+
         class Node
         {
             public readonly TKey Key;
             public TValue Value;
             public Node Next;
+
+            public Node( TKey key, TValue value, Node newNode )
+            {
+                Key = key;
+                Value = value;
+                Next = newNode;
+            }
         }
 
         public int Count => _count;
 
         public void Add( TKey key, TValue value )
         {
+            var index = key.GetHashCode() % _buckets.Length;
+            Node newNode = new Node(key, value, null);
 
+            Node n = _buckets[index];
+            for( ; n.Next != null; n = n.Next );
+            n.Next = newNode;
         }
 
         public void Remove( TKey key )
         {
+            var index = key.GetHashCode() % _buckets.Length;
+
+            Node n = _buckets[index];
+            for( ; !key.Equals( n.Next.Key ); n = n.Next ) ;
+            if ( n.Next != null)
+            {
+                Node prev = n;
+                Node next = n.Next.Next;
+                prev.Next = next;
+            }
 
         }
 
         public TValue this[ TKey key ]
         {
             get { return default( TValue ); }
-            set { }
+            set
+            {
+                var index = key.GetHashCode() % _buckets.Length;
+                Node newNode = new Node( key, value, null );
+
+                Node n = _buckets[index];
+                for( ; n.Next != null; n = n.Next )
+                {
+                    if( key.Equals( n.Key ) )
+                    {
+                        n.Value = value;
+                        return;
+                    }
+                }
+                n.Next = newNode;
+            }
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
